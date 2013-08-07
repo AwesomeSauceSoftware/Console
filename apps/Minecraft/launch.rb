8735342087072a7d8fb813fc6643765173cd78d4
gjgfuj <gjgfuj@gjgfuj-eME642.(none)>
2013-08-07 16:55:20 +1000

    Linux Patch

--- apps/Minecraft/launch.rb
+++ apps/Minecraft/launch.rb
@@ -1,1 +1,2 @@
+#!/usr/bin/env ruby
 `java -jar "C:\\Documents and Settings\\Student\\Application Data\\.minecraft\\minecraft.jar"`
--- apps/Minecraft/launch.sh.rb
+++
@@ -1,1 +0,0 @@
-`java -jar "C:\\Documents and Settings\\Student\\Application Data\\.minecraft\\minecraft.jar"`
